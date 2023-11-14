36fe1c0d0e88b418b1ecd57df67465541e6bab18
Fumihiko Hata <hatappo@users.noreply.github.com>
2023-11-15 04:08:41 +0900

    feat: a command search words with regexp conversion to do it with fuzzy fuzzy

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // console.log('"fuzzy-fuzzy" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fuzzy-fuzzy" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "fuzzy-fuzzy.search",
+    async () => {
+      vscode.window.showInformationMessage("呼ばれた");
+      // vscode.window.showQuickPick([]);
+      const placeHolder = "曖昧に検索したい文字列を半角スペース区切りでどうぞ";
+      const typedText = await vscode.window.showInputBox({
+        placeHolder: placeHolder,
+        value: placeHolder,
+        valueSelection: [0, placeHolder.length],
+      });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fuzzy-fuzzy.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fuzzy!');
-	});
+      if (typedText) {
+        // vscode.window.showInformationMessage(`Got: ${typedText}`);
+        const fuzzyRegexpText = typedText.replaceAll(/ +/g, ".* .*");
+        const aaa = await vscode.commands.executeCommand(
+          "search.action.openNewEditor",
+          {
+            query: fuzzyRegexpText,
+            triggerSearch: true,
+            focusResults: false,
+            regexp: true,
+          }
+        );
+        vscode.window.showInformationMessage(typeof aaa);
+      } else {
+        // vscode.window.showWarningMessage(`Failed to get`);
+      }
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
